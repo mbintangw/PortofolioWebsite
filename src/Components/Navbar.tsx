be1164fa357dfb323a89abe5749fd949a4f6bba4
mbintangw <mbintangw@gmail.com>
2024-07-15 13:31:23 +0700

    style : change size logo and fixed navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,10 +2,10 @@ import { Link } from "react-scroll";
 
 const Navbar = () => {
   return (
-    <nav className="fixed z-50 w-full">
+    <nav className=" z-50 w-full  fixed">
       <div className=" flex items-center justify-between px-4 py-5 mx-auto">
         <div>
-          <h1 className="font-signature text-3xl text-[#DCA63D]">Bintang</h1>
+          <h1 className="font-signature text-5xl text-white">Bintang</h1>
         </div>
         <ul className="flex gap-6 justify-end items-center flex-1 font-bold text-3xl text-white [&>li]:cursor-pointer">
           <li>
