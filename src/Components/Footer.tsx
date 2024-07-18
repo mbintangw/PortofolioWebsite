3a4ef0db1ec4f62cba9c27ef379ad646dbf615e1
mbintangw <mbintangw@gmail.com>
2024-07-18 12:34:26 +0700

    style : responsive for mobile

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,15 +1,15 @@
 const Footer = () => {
   return (
-    <main className="bg-highlight h-[500px]">
+    <main className="bg-highlight lg:h-[500px]">
       <img
         src="/images/cloud.png"
-        className="h-[150px] lg:h-[300px] -rotate-180"
+        className=" lg:h-[300px] -rotate-180"
       />
       <div className="space-y-5 mt-16">
         <h1 className="font-signature text-[25px] md:text-[40px] lg:text-[50px] tracking-widest text-center text-white ">
           Bintang Portofolio
         </h1>
-        <h1 className="text-white text-center text-2xl">
+        <h1 className="text-white text-center text-sm md:text-base lg:text-2xl">
           Created by Mohammad Bintang Wicaksono. All Rights Reserved.
         </h1>
       </div>
