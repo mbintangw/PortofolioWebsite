bdad9845fe45ed880aad2256fd9279672d52401e
mbintangw <mbintangw@gmail.com>
2024-07-18 16:51:06 +0700

    feat : add button github

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import data from "../../data.json";
 import { IoCloseSharp } from "react-icons/io5";
-
+import { TbWorldWww } from "react-icons/tb";
+import { FaGithub} from "react-icons/fa";
 const Project = () => {
   const [selectedProject, setSelectedProject] = useState<any>(null);
   useEffect(() => { document.body.style.overflow = selectedProject ? 'hidden' : 'auto'; }, [selectedProject]);
@@ -29,16 +30,17 @@ const Project = () => {
       
       {selectedProject && (
         <div className="fixed inset-0 flex items-center justify-center bg-black/50 backdrop-blur-xl z-50">
-          <div className="bg-stroke text-white p-10 rounded-lg shadow-lg w-[500px] h-[500px] md:h-[80%] md:w-2/3 lg:w-1/3 overflow-y-auto md:overflow-hidden">
+          <div className="bg-stroke text-white p-10 rounded-lg shadow-lg w-[500px] h-[500px] md:h-[700px] md:w-2/3 overflow-y-auto md:overflow-hidden space-y-10">
             <div className="flex justify-between">
               <h2 className="text-3xl font-bold mb-4">{selectedProject.name}</h2>
               <IoCloseSharp className="cursor-pointer text-3xl text-tertiary" onClick={() => setSelectedProject(null)} />
             </div>
             <img src={selectedProject.poster_path} className="w-full h-64 object-cover mb-4" />
             <p className="mb-4 text-justify text-md md:text-xl text-white/70 ">{selectedProject.description}</p>
-            <div className="space-x-4">
+            <div className="flex gap-4">
               <button className="bg-tertiary text-white px-4 py-2 rounded" onClick={() => setSelectedProject(null)}>Close</button>
-              <button className="bg-highlight text-white px-4 py-2 rounded" onClick={() =>  window.open(selectedProject.website_url, '_blank')} > Demo </button>
+              <button className="bg-highlight text-white px-4 py-2 rounded" onClick={() =>  window.open(selectedProject.website_url, '_blank')} > <TbWorldWww size={30}/> </button>
+              <button className="bg-highlight text-white px-4 py-2 rounded" onClick={() =>  window.open(selectedProject.github_url, '_blank')} > < FaGithub size={30}/> </button>
             </div>
             
           </div>
