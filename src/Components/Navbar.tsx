10d58a178481a388e3e27c2ff36bbac5b78b868c
mbintangw <mbintangw@gmail.com>
2024-07-18 16:26:45 +0700

    style : reponsive for mobile

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,24 +1,80 @@
+import { useState } from "react";
 import { Link } from "react-scroll";
+import { BsList } from "react-icons/bs";
+import { IoCloseSharp } from "react-icons/io5";
 
 const Navbar = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
+  };
+
   return (
-    <nav className=" z-50 w-full  fixed">
-      <div className=" flex items-center justify-between px-4 py-5 mx-auto">
+    <nav className=" z-50 w-full  fixed bg-stroke/30 backdrop-blur-xl ">
+      <div className="flex items-center justify-between px-7 py-5 mx-auto">
         <div>
-          <h1 className="font-signature text-5xl text-white">Bintang</h1>
+          <h1 className="font-signature text-5xl text-white cursor-pointer">
+            <Link to="Home" smooth={true} spy={true}>Bintang</Link>
+            </h1>
         </div>
-        <ul className="flex gap-6 justify-end items-center flex-1 font-bold text-3xl text-white [&>li]:cursor-pointer">
+        <ul className="hidden lg:flex gap-6 justify-end items-center flex-1 font-bold text-3xl text-white [&>li]:cursor-pointer">
           <li>
-            <Link to="home" smooth={true} activeClass="text-blue-500">
+            <Link to="About" smooth={true} spy={true} activeClass="text-blue-500">
               About
             </Link>
           </li>
-          <li>Projects</li>
-          <li>Skill</li>
+          <li>
+            <Link to="Project" smooth={true} spy={true} activeClass="text-blue-500">
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="Skill" smooth={true} spy={true} activeClass="text-blue-500">
+              Skill
+            </Link>
+          </li>
           <li>Resume</li>
         </ul>
+
+        <div className="lg:hidden">
+          {modalOpen ? (
+            <IoCloseSharp
+              className="text-3xl text-white cursor-pointer"
+              onClick={toggleModal}
+            />
+          ) : (
+            <BsList
+              className="text-3xl text-white cursor-pointer"
+              onClick={toggleModal}
+            />
+          )}
+        </div>
+
       </div>
+      {modalOpen && (
+          <ul className="lg:hidden flex flex-col gap-6 -mt-20 justify-center items-center  font-bold text-3xl text-white [&>li]:cursor-pointer h-dvh ">
+            <li>
+              <Link to="About" smooth={true} spy={true} activeClass="text-blue-500">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="Project" smooth={true} spy={true} activeClass="text-blue-500">
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link to="Skill" smooth={true} spy={true} activeClass="text-blue-500">
+                Skill
+              </Link>
+            </li>
+            <li>Resume</li>
+          </ul>
+        )}
+      
     </nav>
+    
   );
 };
 
